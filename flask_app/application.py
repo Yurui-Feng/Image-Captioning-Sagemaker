02eef903d9b928241a6756a68f2b2e7d7b8309c0
Yuri <91753999+Yurui-Feng@users.noreply.github.com>
2023-05-10 21:21:15 -0700

    update resize function to keep original ratio

--- flask_app/application.py
+++ flask_app/application.py
@@ -52,7 +52,7 @@ def allowed_file(filename):
 
 def resize_image(input_image_path, output_image_path, size):
     original_image = Image.open(input_image_path)
-    resized_image = original_image.resize(size)
+    resized_image = original_image.thumbnail(size)
     resized_image.save(output_image_path)
 
 @application.route('/uploads/<filename>')
